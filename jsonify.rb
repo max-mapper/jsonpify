36e56b897cc92584ff55f2cf6291e8096ace0ac7
Max Ogden <max@maxogden.com>
2010-09-14 19:25:37 -0700

    adding readme

--- jsonify.rb
+++ jsonify.rb
@@ -1,13 +1,8 @@
 get '/' do
-  'hay guys'
+  haml :index
 end
 
-get "/:resource" do
-  content_type :json
+get "/jsonpify" do
   response = HTTParty.get(params[:resource]) rescue "{error:'Error!'}"
   "#{params[:callback]}(#{Crack::JSON.parse(response).to_json})"
-end                                                                                      
-  
-  
-  
-  
+end
