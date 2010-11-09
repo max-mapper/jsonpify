7b8daf56a01742564c44736aecd5643730055867
Max Ogden <max@maxogden.com>
2010-11-09 14:27:04 -0800

    making it functional

--- config.ru
+++ config.ru
@@ -2,5 +2,7 @@ require 'sinatra'
 require 'jsonify'
 require 'httparty'
 require 'crack'
-
+require 'haml'
+require 'json'
+require 'ap'
 run Sinatra::Application
--- jsonify.rb
+++ jsonify.rb
@@ -1,8 +1,12 @@
 get '/' do
-  haml :index
-end
-
-get "/jsonpify" do
-  response = HTTParty.get(params[:resource]) rescue "{error:'Error!'}"
-  "#{params[:callback]}(#{Crack::JSON.parse(response).to_json})"
+  if params[:resource]
+    content_type :json
+    httparty_error = {"error" => "HTTParty Error!"}.to_json
+    return {"error" => "You must specify a callback parameter for JSONP"}.to_json unless params[:callback]
+    response = HTTParty.get(params[:resource]) rescue httparty_error
+    response = response.parsed_response rescue httparty_error
+    return "#{params[:callback]}(#{response.to_json})"
+  else
+    haml :index
+  end
 end
