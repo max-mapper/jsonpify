1b7201e4a41fa9a1f2afac2456a79f6677c579cd
Max Ogden <max@maxogden.com>
2010-11-30 16:56:45 -0800

    changing symbols to strings... stupid ruby

--- jsonpify.rb
+++ jsonpify.rb
@@ -1,10 +1,10 @@
 get '/' do
-  if params[:resource]
+  if params['resource']
     content_type :json
     httparty_error = {"error" => "HTTParty Error!"}.to_json
-    return {"error" => "You must specify a callback parameter for JSONP"}.to_json unless params[:callback]
-    resource = params.delete(:resource)
-    callback = params.delete(:callback)
+    return {"error" => "You must specify a callback parameter for JSONP"}.to_json unless params['callback']
+    resource = params.delete('resource')
+    callback = params.delete('callback')
     response = HTTParty.get(resource, :query => params) rescue httparty_error
     response = response.parsed_response rescue httparty_error
     return "#{callback}(#{response.to_json})"
